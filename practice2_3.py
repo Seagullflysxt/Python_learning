0366a8e89a5e6956cff8c4935bd13753e6e1b2c7
Seagullflysxt <89381353+Seagullflysxt@users.noreply.github.com>
2021-08-23 16:28:28 +0800

    Delete practice2_3.py

--- practice2_3.py
+++
@@ -1,6 +0,0 @@
-#practice2_3
-name = "eric"
-message = f"Hello {name.title()},would you like to learn some Python today?"
-#message
-print(message)
-print(f"hello {name.title()},would you like to learn some Python today?")
