129c35aabe2eb8291918b0f32b0c9ad97698d9a7
Seagullflysxt <89381353+Seagullflysxt@users.noreply.github.com>
2021-08-23 16:42:20 +0800

    Add files via upload

---
+++ python_lerning/python_crash_course/chapter_2/practice2_4.py
@@ -0,0 +1,8 @@
+#practice2_4
+name1 = "taylor swift"
+name2 = "wAtEr"
+print(name1)
+print(name2)
+print(name2.lower())
+print(name1.upper())
+print(name1.title())
---
+++ python_lerning/python_crash_course/chapter_2/practice2_5.py
@@ -0,0 +1,3 @@
+#practice2_5*****
+name = "Albert Einstein"
+print(f'{name} onec said,"A person who never made a mistake never tried anything new."')
