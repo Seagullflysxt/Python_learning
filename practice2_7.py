1be64cf20cbdae2706c42b7bbcffcea1f645b62b
Seagullflysxt <89381353+Seagullflysxt@users.noreply.github.com>
2021-08-23 16:21:43 +0800

    Add files via upload

---
+++ practice2_7.py
@@ -0,0 +1,6 @@
+#practice2_7
+name = "\tada lovelace\n"
+print(name)
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
---
+++ practice2_9.py
@@ -0,0 +1,5 @@
+#5.2 2021
+#use a variable to present your favorite number,then use the variable to construct a message which point out what's your favorite number,then print 
+fav_num = 1
+message = f"My favorite number is {fav_num}"
+print(message)
